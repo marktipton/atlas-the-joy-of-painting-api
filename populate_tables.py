920e74511545cfa85cd367e21a82b0105b07f2a5
marktipton <marktipton95@gmail.com>
2024-07-25 15:11:51 -0500

    aowegn

--- populate_tables.py
+++ populate_tables.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import psycopg2
+from psycopg2 import sql
 
 c = psycopg2.connect(
     dbname="joyofcoding",
@@ -14,9 +15,48 @@ c = psycopg2.connect(
 # open cursor to perform operations on database
 cursor = c.cursor()
 
+# Create the episodes table if it doesn't exist
+create_table_query = """
+CREATE TABLE IF NOT EXISTS episodes (
+    id SERIAL PRIMARY KEY,
+    painting_index INTEGER,
+    img_src VARCHAR,
+    title VARCHAR,
+    season INTEGER,
+    episode_number INTEGER,
+    youtube_src VARCHAR,
+    date DATE,
+    month INTEGER,
+    day INTEGER,
+    year INTEGER,
+    note TEXT
+);
+"""
+cursor.execute(create_table_query)
+
 # load data into pandas DataFrames
-df_colors = pd.read_csv('bob_ross_colors.csv')
-df_subjects = pd.read_csv('bob_ross_subjects.csv')
+df_episodes = pd.read_csv('bob_ross_colors_with_dates.csv')
+# df_subjects = pd.read_csv('bob_ross_subjects.csv')
+
+# Insert DataFrame records one by one into the episodes table
+for index, row in df_episodes.iterrows():
+    insert_query = sql.SQL("""
+    INSERT INTO episodes (painting_index, img_src, title, season, episode_number, youtube_src, date, month, day, year, note)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """)
+    cursor.execute(insert_query, (
+        row['painting_index'],
+        row['img_src'],
+        row['painting_title'],
+        row['season'],
+        row['episode'],
+        row['youtube_src'],
+        row['date'],
+        row['month'],
+        row['day'],
+        row['year'],
+        row['note']
+    ))
 
 # commit changes to db
 c.commit()
